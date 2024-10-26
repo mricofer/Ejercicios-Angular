569f16dfa2d18f43c7141f73fa911972bf7b80ba
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 18:47:44 +0200

    ✨ Finish ejercicio6 typescript

--- src/app/ejercicio6/ejercicio6.component.ts
+++ src/app/ejercicio6/ejercicio6.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-ejercicio6',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './ejercicio6.component.html',
   styleUrl: './ejercicio6.component.css'
 })
-export class Ejercicio6Component {
 
+export class Ejercicio6Component{
+  // Propiedades para recibir datos de entrada del producto
+  @Input() nombre: string = '';
+  @Input() descripcion: string = '';
+  @Input() precio: number = 0;
 }
