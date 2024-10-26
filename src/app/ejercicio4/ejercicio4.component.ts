36eebb47d967bca0260bce7cd400a7d359ae0590
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 18:13:23 +0200

    🚧 Create exercise 4

---
+++ src/app/ejercicio4/ejercicio4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio4',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio4.component.html',
+  styleUrl: './ejercicio4.component.css'
+})
+export class Ejercicio4Component {
+
+}
