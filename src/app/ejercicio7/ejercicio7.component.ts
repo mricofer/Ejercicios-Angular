bd6eb86ef83712ea89c21c7d5de5b1b5d2821c89
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 19:18:50 +0200

    ✨ Finish ejercicio7 typescript

--- src/app/ejercicio7/ejercicio7.component.ts
+++ src/app/ejercicio7/ejercicio7.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio7',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './ejercicio7.component.html',
   styleUrl: './ejercicio7.component.css'
 })
 export class Ejercicio7Component {
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number | string = '';
 
+  sumar() {
+    this.resultado = this.num1 + this.num2;
+  }
+
+  restar() {
+    this.resultado = this.num1 - this.num2;
+  }
+
+  multiplicar() {
+    this.resultado = this.num1 * this.num2;
+  }
+
+  dividir() {
+    if (this.num2 !== 0) {
+      this.resultado = this.num1 / this.num2;
+    } else {
+      this.resultado = 'Error: División por cero';
+    }
+  }
+
+  raizCuadrada() {
+    this.resultado = Math.sqrt(this.num1);
+  }
 }
