3cc103ee75d2d98fa5a2011bb31482c0f74e0319
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 18:43:09 +0200

    ✨ Finish ejercicio5 typescript

--- src/app/ejercicio5/ejercicio5.component.ts
+++ src/app/ejercicio5/ejercicio5.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-ejercicio5',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './ejercicio5.component.html',
   styleUrl: './ejercicio5.component.css'
 })
-export class Ejercicio5Component {
+export class Ejercicio5Component  {
+  nuevoComentario: string = ''; // Almacena el comentario que el usuario está ingresando
+  listaComentarios: string[] = []; // Lista de todos los comentarios ingresados
 
+  // Método para agregar un nuevo comentario a la lista
+  enviarComentario() {
+    if (this.nuevoComentario.trim()) {
+      this.listaComentarios.push(this.nuevoComentario); // Agrega el comentario a la lista
+      this.nuevoComentario = ''; // Limpia el campo de entrada
+    }
+  }
 }
