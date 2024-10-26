da674e6423e614957647184d352d3f0449632cc4
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 18:20:26 +0200

    🚧 Create exercise 6

---
+++ src/app/ejercicio6/ejercicio6.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio6',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio6.component.html',
+  styleUrl: './ejercicio6.component.css'
+})
+export class Ejercicio6Component {
+
+}
