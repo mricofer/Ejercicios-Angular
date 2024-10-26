26dbdf3956e670aa161584de44fdd3a4e1b138fa
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 18:12:06 +0200

    ➕ Add dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Ejercicio1Component} from './ejercicio1/ejercicio1.component';
+import {Ejercicio2Component} from './ejercicio2/ejercicio2.component';
+import {Ejercicio3Component} from './ejercicio3/ejercicio3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Ejercicio1Component, Ejercicio2Component, Ejercicio3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
