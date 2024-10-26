b937d1eebbc58fe43a04bbe026d14bb78f867ed3
Miguel Rico <mricofernandez@safareyes.es>
2024-10-26 19:45:45 +0200

    ✨ Finish typescript logic from ejercicio8

--- src/app/ejercicio8/ejercicio8.component.ts
+++ src/app/ejercicio8/ejercicio8.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+
+interface Articulo {
+  nombre: string;
+  precio: number;
+  cantidad: number;
+}
 
 @Component({
   selector: 'app-ejercicio8',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './ejercicio8.component.html',
   styleUrl: './ejercicio8.component.css'
 })
 export class Ejercicio8Component {
+  articulos: Articulo[] = [
+    { nombre: 'Manzana', precio: 0.5, cantidad: 0 },
+    { nombre: 'Plátano', precio: 0.3, cantidad: 0 },
+    { nombre: 'Naranja', precio: 0.4, cantidad: 0 },
+    { nombre: 'Fresa', precio: 0.6, cantidad: 0 },
+  ];
 
+  // Método para aumentar la cantidad de un artículo
+  aumentarCantidad(articulo: Articulo) {
+    articulo.cantidad++;
+  }
+
+  // Método para disminuir la cantidad de un artículo
+  disminuirCantidad(articulo: Articulo) {
+    if (articulo.cantidad > 0) {
+      articulo.cantidad--;
+    }
+  }
+
+  // Método para calcular el total
+  calcularTotal(): number {
+    return this.articulos.reduce((total, articulo) => total + (articulo.precio * articulo.cantidad), 0);
+  }
 }
